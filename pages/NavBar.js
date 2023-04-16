e1e626b13478b3f0d004cb3b58952d4745ad6f9e
sjh9391985 <sjh9391985@gmail.com>
2023-04-16 18:48:49 +0900

    feat: css module 사용방법

--- pages/NavBar.js
+++ pages/NavBar.js
@@ -1,10 +1,9 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
+import styles from "./NavBar.module.css"
 
 export default function NavBar(){
-    const router = useRouter();
     return (
-            <nav>
+            <nav className={styles.nav}>
                 <Link href="/">
                     Home
                 </Link>
