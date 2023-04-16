47a037ced5e80a8a63d80c1717c06e790903203b
sjh9391985 <sjh9391985@gmail.com>
2023-04-16 18:25:18 +0900

    feat: router 사용방법

---
+++ pages/NavBar.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar(){
+    const router = useRouter();
+    return (
+            <nav>
+                <Link href="/">
+                    Home
+                </Link>
+                <Link href="about">
+                    About
+                </Link>
+            </nav>
+            ) 
+
+}
